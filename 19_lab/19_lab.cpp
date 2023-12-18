a0c486bb54ad842e60142e49ccfdde1344be5b78
kasiyan16 <146705728+kasiyan16@users.noreply.github.com>
2023-12-18 21:08:43 +0000

    000

--- 19_lab/19_lab.cpp
+++ 19_lab/19_lab.cpp
@@ -1,31 +1,51 @@
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 
-// Функція з варіанту
-double myFunction(double x) {
-    // Замініть це виразом із вашого варіанту
-    return 9.2 * cos(2 * x) - fabs(sin(x) / 1.1);
+using namespace std;
+
+double customFunction(double x) {
+    return cos(pow(x, 2) + 1) - abs(sin(2 * x) - 5.76);
 }
 
 int main() {
-    const int k_values[] = {1, 2, 3, 4};
-    const int n_values[] = {1, 2, 3, 4};
+    const int size = 4;
 
-    // Виведемо заголовок таблиці
-    std::cout << "№ п/п\tfn(x)\n";
+    double matrix[size][size];
 
-    int count = 1;
-
-    // Проходимо по комбінаціях k і n
-    for (int k : k_values) {
-        for (int n : n_values) {
-            // Обчислюємо fn(x) для кожної комбінації
-            double result = myFunction(k + 20 * n + sin(k + 21 * n));
-
-            // Виводимо результат у вигляді таблиці
-            std::cout << count++ << "\t" << result << std::endl;
+    for (int n = 0; n < size; ++n) {
+        for (int k = 0; k < size; ++k) {
+            double a = customFunction(n * 13.4) * customFunction(abs(k + 1)) * customFunction(pow(k + 1, 2) - 2.25) +
+                       customFunction(sin(k + 1)) * abs(customFunction(cos(pow(n + 1, 2) - 3.8))) / 4.5 - 9.7 * customFunction(n + 1 - 3.1);
+            matrix[n][k] = a;
         }
     }
+
+    cout << "Matrix:" << endl;
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            cout << setw(10) << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    // Calculate the sum of diagonal elements and count of negative elements
+    double diagonalSum = 0.0;
+    int negativeCount = 0;
+
+    for (int i = 0; i < size; ++i) {
+        diagonalSum += matrix[i][i]; // Sum of diagonal elements
+
+        for (int j = 0; j < size; ++j) {
+            if (matrix[i][j] < 0) {
+                negativeCount++;
+            }
+        }
+    }
+
+    // Output the results
+    cout << "Sum of diagonal elements: " << diagonalSum << endl;
+    cout << "Count of negative elements: " << negativeCount << endl;
 
     return 0;
 }
